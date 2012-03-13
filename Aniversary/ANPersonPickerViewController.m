4261fdfc88f794d45e42a192516a825259b43861
Chih-Wei Lee <dlackty@gmail.com>
2012-03-13 11:40:38 +0800

    Create mock person picker view controller.

---
+++ Aniversary/ANPersonPickerViewController.m
@@ -0,0 +1,35 @@
+//
+//  ANPersonPickerViewController.m
+//  Aniversary
+//
+//  Created by Lee Chih-Wei on 3/13/12.
+//  Copyright (c) 2012 Polydice, Inc. All rights reserved.
+//
+
+#import "ANPersonPickerViewController.h"
+
+@interface ANPersonPickerViewController ()
+
+@end
+
+@implementation ANPersonPickerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload {
+  [super viewDidUnload];
+  // Release any retained subviews of the main view.
+}
+
+@end
