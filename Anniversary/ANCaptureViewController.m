7bfd70900b9e3f95878e19a69889463e18729695
LicL <98703019@nccu.edu.tw>
2012-03-28 21:06:03 +0800

    Add new ANCaptureViewController

--- Anniversary/ANCaptureViewController.m
+++ Anniversary/ANCaptureViewController.m
@@ -1,83 +1,43 @@
 //
-// ANCaptureViewController.m
-// Anniversary
+//  ANCaptureViewController.m
+//  Anniversary
 //
-// Created by Devi Eddy on 03/14/12.
-// Copyright (c) 2012 Polydice, Inc. All rights reserved.
+//  Created by Devi Eddy on 03/28/12.
+//  Copyright (c) 2012 Polydice, Inc. All rights reserved.
 //
 
 #import "ANCaptureViewController.h"
-#import "ANPersonPickerViewController.h"
 
 @interface ANCaptureViewController ()
 
 @end
 
 @implementation ANCaptureViewController
-@synthesize imageView, messageField, personPicker, takePhoto, editText;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-  if (self) {
-    // Custom initialization
-  }
-  return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
 }
 
--(IBAction)newPerson:(id)sender{
-  ANPersonPickerViewController *personPickerView = [[ANPersonPickerViewController alloc] initWithNibName:nil bundle:nil];
-  personPickerView.modalTransitionStyle = UIModalTransitionStylePartialCurl;
-  [self.navigationController pushViewController:personPickerView animated:YES];
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
 }
 
--(IBAction)capture:(id)sender{
-  UIImagePickerController *picker = [[UIImagePickerController alloc] init];
-  picker.delegate = self;
-  picker.allowsEditing = YES;
-  //picker.sourceType = UIImagePickerControllerSourceTypeCamera;
-  picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
-  [self presentModalViewController:picker animated:YES];
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
 }
 
-- (IBAction)textEditor:(id)sender{
-  // UIAlertView
-  
-  UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Title" message:nil delegate:self cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-  alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
-  [alertView show];
-  
-  
-}
-
-#pragma mark - UIImagePickerControllerDelegate
-
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
-  [picker dismissModalViewControllerAnimated:YES];
-  imageView.image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
-}
-
-#pragma mark - UIAlertViewDelegate
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-  NSLog(@"Index %d", buttonIndex);
-  
-  UITextField *field = [alertView textFieldAtIndex:0];
-  messageField.text = [NSString stringWithFormat:@"%@", field.text];
-  NSLog(@"Field %@", field.text);
-}
-
-#pragma mark - Private
-
-- (void)doneButtonClicked:(id)sender {
-  [self dismissModalViewControllerAnimated:YES];
-}
-
-- (void)viewDidLoad {
-  [super viewDidLoad];
-  self.navigationController.navigationBar.tintColor = [UIColor brownColor ];
-  
-  self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneButtonClicked)];
-  self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneButtonClicked:)];
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
